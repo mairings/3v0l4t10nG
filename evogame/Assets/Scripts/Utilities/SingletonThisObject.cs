5346edc6a82e72d42fde18baa85f5f63c80f1ca9
mairings <eyupylmzz@gmail.com>
2021-12-05 18:12:48 +0300

    Urp Added
    Post Processing install and basic settings
    Singleton script created

---
+++ evogame/Assets/Scripts/Utilities/SingletonThisObject.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EvoGame.Utilities
+{
+    public abstract class SingletonThisObject<T> : MonoBehaviour
+    {
+        public static T Instance { get; private set; }
+
+        protected void SingletonThisObjectFunc(T entity)
+        {
+            if (Instance == null)
+            {
+                Instance = entity;
+                DontDestroyOnLoad(this.gameObject);
+            }
+            else
+            {
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
+
+
